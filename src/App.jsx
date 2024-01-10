bd5500be42f558e7b4ac1c3e18084671dea229d3
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-01-10 23:14:26 +0900

    81. Lifting State Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 import './index.css';
 
 function App() {
+  // 81. Lifting State Up (State(상태) 끌어올리기)
+  // 현재 진행 중인 플레이어 제어
+  // 'X' 기호에 할당된 플레이어가 첫 번째 플레이어가 되도록 초기화
+  // setActivePlayer 함수를 사용하여 플레이어를 변경
+  const [ activePlayer, setActivePlayer ] = useState('X');
+
+  // 차례가 바뀔 때마다 플레이어 변경 함수
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
       LOG
     </main>
